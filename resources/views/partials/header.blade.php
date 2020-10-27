7ffa8a11d5e1863da01fd0bb7c809e64abbd4ee5
Matt Daneshvar <matt@4thirteen.com.my>
2020-10-27 17:21:45 +0800

    Format views

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,6 +2,7 @@
     <div class="container flex items-center justify-center">
         <a href="{{ route('home') }}" class="">
             Issue Duplicator for
+
             <a target="_blank" href="https://github.com">
                 <img src="{{ url('img/github-logo.png') }}" alt="Github Logo" class="w-16 ml-1">
             </a>
