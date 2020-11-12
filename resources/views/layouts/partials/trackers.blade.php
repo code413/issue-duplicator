fc6e59914dd4f8459e0b2f38daa92efe7f468de2
Matt Daneshvar <matt@4thirteen.com.my>
2020-11-12 16:45:32 +0800

    Fix typo in config key

--- resources/views/layouts/partials/trackers.blade.php
+++ resources/views/layouts/partials/trackers.blade.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google-analytics.id')  }}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google_analytics.id')  }}"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', '{{ config('services.google-analytics.id')  }}');
+    gtag('config', '{{ config('services.google_analytics.id')  }}');
 </script>
